a17095b6aa7ed89cf49949721b54f354553f29c6
Anastasia <slyadneva.anastasia@gmail.com>
2025-11-14 13:10:53 -0600

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project-management-react/",
   plugins: [
     devtools(),
     tanstackRouter({
